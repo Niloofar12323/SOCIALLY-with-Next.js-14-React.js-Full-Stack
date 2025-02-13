2986ed5f0e31c46c1ad082f8f42f5547933f844c
niloofar yousefiafrouz <niloofar.yousefiafrouz@dci-student.org>
2025-02-13 01:16:45 +0100

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+
+import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-  <div>
+  <div className="m-4">
    <SignedOut>
             <SignInButton  mode="modal">
              <Button>Sign in</Button>
@@ -12,6 +14,8 @@ export default function Home() {
           <SignedIn>
             <UserButton />
           </SignedIn>
+      <ModeToggle />
+          <Button variant={"secondary"}>Click me</Button>
   </div>
   );
 }
